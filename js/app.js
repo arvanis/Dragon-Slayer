26d24ba1e84adb00a3ac62d70dd3ec09e84246f7
arvanis <chyrek.kamil@gmail.com>
2016-07-13 16:29:25 +0200

    add styles

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
 
     
+    var msg = $("#message"),
+        wojownik = $("<p>Wojownik dysponuje ogromną siłą, przez co jego trafienia mogą zadawać większe obrażenia. Nie posiada pancerza, a jego dwuręcznym mieczem niestety nie wyprowadza się zbyt szybkich ataków.</p>"),
+        rycerz = $("<p>Rycerz jest silny i wytrzymały, jednak ciężka zbroja znacznie ogranicza jego ruchy.</p>"),
+        lowca = $("<p>Łowca, dzięki lekiej zbroi schowanej pod płaszczem, z łatwością porusza się po polu bitwy, wyprowadzając szybkie i celne ataki oraz unikając uderzeń smoka. Niestety, nie jest zbyt silny i wytrzymały.</p>");
+    
 // Postacie i ekwipunek
     
     function character(str, dex, hp, def) {
@@ -37,6 +42,7 @@ $(document).ready(function() {
             hp = barbarian.healthPoints;
             def = barbarian.defence;
             
+            msg.prepend(wojownik);
             console.log ("brb: " + str + " " + dex + " " + hp + " " + def);
         }
         else if ( choosenOne == "knight" ) {
@@ -45,6 +51,7 @@ $(document).ready(function() {
             hp = knight.healthPoints;
             def = knight.defence;
             
+            msg.prepend(rycerz);
             console.log ("knight: " + str + " " + dex + " " + hp + " " + def);
         }
         else if ( choosenOne == "rogue" ) {
@@ -53,6 +60,7 @@ $(document).ready(function() {
             hp = rogue.healthPoints;
             def = rogue.defence;
             
+            msg.prepend(lowca);
             console.log ("rogue: " + str + " " + dex + " " + hp + " " + def);
         };
     });
@@ -63,16 +71,26 @@ $(document).ready(function() {
         dragonHp = dragon.healthPoints,
         dragonBar = $("#dragonHpLeft");
         
+    
+    
     // Atak ze strony gracza
     
+    var spotHead = $("<p>Są niewielkie szanse, że bohaterowi uda się trafić w głowę smoka, aczkolwiek silne uderzenie mogłoby mu poważnie zaszkodzić.</p>"),
+        spotBody = $("<p>Pomiędzy łuskami na brzuchu znajduje się odsłonięte ciało smoka, jednak naprawdę wątpię, by bohater dał radę w nie trafić.</p>"),
+        spotHand = $("<p>Myślę, że są duże szanse na trafienie w jego łapę i może go to zaboleć.</p>"),
+        spotLeft = $("<p>Niełatwo trafić w trzon jego skrzydła, ale to jeden z jego bardziej czułych punktów.</p>"),
+        spotRight = $("<p>Bohater bez problemu trafi w jego skrzydło, ale raczej nie wyrządzi mu tym większej krzywdy.</p>");
+    
+    
+
+    
+    
+    
+    
     attack.on("click", function() {  
+        
         attack.hide();
         console.log("Attack function locked");
-        setTimeout(attackUnlocked, 4000 - (40 * dex));
-        function attackUnlocked() {
-            attack.fadeIn();
-            console.log("Attack function unlocked");
-        };
         
                    
         var dexDuel = dex - dragon.dexterity,
@@ -89,13 +107,28 @@ $(document).ready(function() {
             if ( damage > 0 ) {
                 dragonHp -= damage;    
                 console.log("Trafiłeś: " + damage + ". Smokowi zostało jeszcze: " + dragonHp + " punktów życia!");
+                
+                if ( damage <= 20 ) {
+                    msg.prepend($("<p>Bohater trafił, ale smok prawie tego nie poczuł.</p>"));
+                }
+                else if ( (damage > 20) && (damage <= 50) ) {
+                    msg.prepend($("<p>Niezły cios!</p>"));
+                }
+                else if ( (damage > 50) && (damage <= 100) ) {
+                    msg.prepend($("<p>To go musiało zaboleć! Potężne trafienie!</p>"));
+                }
+                else if ( damage > 100 ) {
+                    msg.prepend($("<p>Niesamowity atak!</p>"));
+                };
             }
             else {
-                console.log("Trafiłeś, ale nie zadałeś żadnych obrażeń!");
-            }
+                console.log("Twój bohater trafił, ale nie zadał żadnych obrażeń!");
+                msg.prepend($("<p>Twój bohater trafił, ale nie zadał żadnych obrażeń!</p>"));
+            };
         }
         else {
             console.log("Nie trafiłeś");
+            msg.prepend($("<p>Twój bohater nie trafił!</p>"));
         }
         var HpValue = (100 * (dragonHp / 1000)) + "%";
         dragonBar.width(HpValue);
@@ -114,6 +147,39 @@ $(document).ready(function() {
             playerBar.width("100%");
             entrance.fadeOut();
             console.log("Twoje aktualne zdrowie " + playerHp + " przy sile: " + str + " przeciwko sile smoka: " + dragon.strength);
+            
+            var spots = setInterval(attackUnlocked, 3000 - (20 * dex));
+            function attackUnlocked() {
+                var easy = Math.random();
+                
+                if ( easy < 0.5 ) {
+                    $("#dhand + .crosshair").fadeIn("fast");
+                }
+                else if ( easy >= 0.5 ) {
+                    $("#drightWing + .crosshair").fadeIn("fast");
+                };
+                
+                var hard = Math.random();
+                
+                if ( hard < 0.3 ) {
+                    $("#dbody + .crosshair").fadeIn("fast");
+                }
+                else if ( (hard >= 0.3) && (hard < 0.6) ) {
+                    $("#dhead + .crosshair").fadeIn("fast");
+                }
+                else if ( hard >= 0.6) {
+                    $("#dleftWing + .crosshair").fadeIn("fast");
+                };
+                
+                setTimeout(noSpots, 30 + (10 * dex));
+                
+                function noSpots() {
+                    attack.fadeOut("fast");
+                };
+            
+                console.log("Attack function unlocked");
+            };
+            
             var dragonFury = setInterval(dragonAttack, 10000);
                 
             function dragonAttack() {
@@ -124,7 +190,7 @@ $(document).ready(function() {
                     
                     var luck = Math.random();
                     
-                    console.log ("Próba smoka: " + hit + " przeciwko Twojemu szczęsciu: " + luck);
+                    console.log ("Próba smoka: " + hit + " przeciwko Twojemu szczęściu: " + luck);
                     
                     if ( hit > luck ) {
                         var damage = Math.floor(dragon.strength * luck) - def;
@@ -132,10 +198,26 @@ $(document).ready(function() {
                         if ( damage > 0 ) {
                             playerHp -= damage;    
                             console.log("Smok trafił: " + damage + ". Zostało Ci jeszcze: " + playerHp + " punktów życia!");
+                            
+                            if ( damage <= 20 ) {
+                                msg.prepend($("<p>Uderzenie smoka ledwo dosięgło Twojego bohatera, ale zadało mu trochę obrażeń.</p>"));
+                            }
+                            else if ( (damage > 20) && (damage <= 50) ) {
+                                msg.prepend($("<p>Twój bohater otrzymał mocne uderzenie.</p>"));
+                            }
+                            else if ( (damage > 50) && (damage <= 100) ) {
+                                msg.prepend($("<p>Smok zionął ogniem! Straszliwy atak!</p>"));
+                            }
+                            else if ( damage > 100 ) {
+                                msg.prepend($("<p>Smok wzbił się w powietrze i runął całym cielskiem na Twojego bohatera.</p>"));
+                            };            
+                            
+                            
                             var HpValue = (100 * (playerHp / playerInitialHp)) + "%";
                             playerBar.width(HpValue);
                             if ( playerHp <= 0 ) {
                                 console.log("Poległeś!");
+                                msg.prepend($("<p>Twój śmiałek zginął!</p>"));
                                 entrance.fadeIn();
                                 
                                 $("input[type=radio]:checked").parent().remove();
@@ -147,28 +229,35 @@ $(document).ready(function() {
                                 
                                 if (isEmpty($('.fireplace'))) {
                                     call.hide();
-                                    $("#allDead").fadeIn();
+                                    msg.prepend($("<p>Wszyscy śmiałkowie zginęli. Być może później będziesz mógł spotkać przy ognisku nowych śmiałków. Tymczasem zmęczony walką smok zasnął w swojej pieczarze i nie zagraża okolicznym krainom.</p>"));
                                 }
                                 else {
-                                    $("#playerDead").fadeIn();
+                                    msg.prepend($("<p>Twój śmiałek poległ. Możesz jednak wykorzystać zmęczenie smoka po walce i poprosić kolejnego bohatera, by dokończył dzieła. Udaj się do ogniska.</p>"));
                                 }
+                                
+                                clearInterval(dragonFury);
+                                clearInterval(spots);
                             };
                         }
                         else {
                             console.log("Smok trafił, ale nie zadał Ci żadnych poważnych obrażeń!");
+                            msg.prepend($("<p>Smok trafił, ale nie zadał Twojemu bohaterowi żadnych poważnych obrażeń!</p>"));
                         }
                     }
                     else {
                         console.log("Smok nie trafił!");
+                        msg.prepend($("<p>Twój bohater uchylił się przed atakiem smoka!</p>"));
                     }
                 }
                 else if ( (dragonHp <= 0) && (playerHp > 0) ) {
                     clearInterval(dragonFury);
+                    clearInterval(spots);
                 };
             }
         }
         else {
             console.log("Wybierz najpierw śmiałka");
+            msg.prepend($("<p>Przed wywołaniem smoka lepiej poproś jakiegoś śmiałka, by wqalczył w Twoim imieniu.</p>"));
         }
         
     });
@@ -184,6 +273,7 @@ $(document).ready(function() {
     var navButtons = $(".navigation");
     $(".tabs").hide()
     $("#tab_1").fadeIn();
+ 
     
     navButtons.on("click", function() {
         var i = $(this).data("id");
@@ -192,6 +282,23 @@ $(document).ready(function() {
         function tabDisplay() {
             var tab = "#tab_" + i;
             $(tab).fadeIn();
+            
+            msg.prepend(tabsTable[i - 1]);
+            
         };
     })
+    
+    
+    
+    
+// Wiadomości
+        
+    var tab1 = $("<p>W tym miejscu władca tej krainy obwieszcza swoje najnowsze dokonania.</p>"),
+        tab2 = $("<p>Jeden z królewskich skrybów opisał w tym miejscu problem, z jakim przyszło się nam borykać.</p>"),
+        tab3 = $("<p>Przy ognisku możesz poprosić jednego ze śmiałków, by walczył w Twoim imieniu ze smokiem.</p>"),
+        tab4 = $("<p>W tej jaskini swoje legowisko ma smok. Jeśli jesteś pewien, że chcesz z nim walczyć, wystarczy go zawołać.</p>");
+    
+    var tabsTable = [tab1, tab2, tab3, tab4];
+
+        
 });
